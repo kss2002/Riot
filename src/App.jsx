798e63cd3d94893bac2c52d80f32ba3f7e4856e1
seong su kim <know.warehouse02@gmail.com>
2025-06-19 17:55:57 +0900

    project rest

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,8 @@
-import Counter from '../src/page/Counter.jsx';
-import ProfileCard from '../src/page/ProfileCard.jsx';
-import UserList from './effect/UserList.jsx';
-import DarkModeToggle from './state/DarkModeToggle.jsx';
-import InputForm from './state/InputForm.jsx';
-
 export default function App() {
   return (
     <>
-      <div>
-        <h1>Props vs State 예제</h1>
-        <Counter title="클릭 카운터" />
-      </div>
-      <div>
-        <h1>프로필 카드 예제</h1>
-        <ProfileCard />
-      </div>
-      <div>
-        <h1>InputForm 예제</h1>
-        <InputForm />
-      </div>
-      <div>
-        <h1>DarkModeToggle 예제</h1>
-        <DarkModeToggle />
-      </div>
-      <hr />
-      <div>
-        <h1>useEffect 예제</h1>
-        <UserList />
-      </div>
+      <h1>Welcome to the Apple App</h1>
+      <p>This is a simple React application.</p>
     </>
   );
 }
